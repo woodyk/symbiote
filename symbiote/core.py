5769601f0688e94c14766b8ce4e9ec224e034d6d
Kato <kato@smallroom.com>
2023-11-22 18:42:37 -0500

    changes

--- symbiote/core.py
+++ symbiote/core.py
@@ -18,7 +18,7 @@ class symbiotes:
     def __init__(self, settings):
         # Available models
         self.models = {
-            "gpt-4": 8192,
+            "gpt-4": 128000,
             "gpt-3.5-turbo-16k": 16000,
             "gpt-4-32k": 32768,
             "gpt-3.5-turbo": 4096,
